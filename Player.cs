23467af2aec0c4fb613e467f48c8774214824a39
Brad Beise <bradleybeise@gmail.com>
2024-12-05 22:00:30 -0500

    Adding sprint

--- Player.cs
+++ Player.cs
@@ -2,7 +2,9 @@ using Godot;
 
 public partial class Player : Godot.CharacterBody3D
 {
-    public const float Speed = 5.0f;
+    public float CurrentSpeed = 15.0f;
+    public const float ConstSpeed = 10.0f;
+    public const float SprintSpeed = 15.0f;
     public const float JumpVelocity = 4.5f;
 
 
@@ -21,19 +23,28 @@ public partial class Player : Godot.CharacterBody3D
         if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
             velocity.Y = JumpVelocity;
 
+        if (Input.IsActionPressed("sprint"))
+        {
+            CurrentSpeed = SprintSpeed;
+        }
+        else
+        {
+            CurrentSpeed = ConstSpeed;
+        }
+
         // Get the input direction and handle the movement/deceleration.
         // As good practice, you should replace UI actions with custom gameplay actions.
         Vector2 inputDir = Input.GetVector("left", "right", "forward", "back");
         Vector3 direction = (new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
         if (direction != Vector3.Zero)
         {
-            velocity.X = direction.X * Speed;
-            velocity.Z = direction.Z * Speed;
+            velocity.X = direction.X * CurrentSpeed;
+            velocity.Z = direction.Z * CurrentSpeed;
         }
         else
         {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, CurrentSpeed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, CurrentSpeed);
         }
 
         Velocity = velocity;
