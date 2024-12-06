53bdb92d265137015b8d517b10ad7382a49f24b5
Brad Beise <bradleybeise@gmail.com>
2024-12-05 20:04:25 -0500

    Refactoring code and structure

--- Rotation.cs
+++ Rotation.cs
@@ -1,13 +1,22 @@
 using Godot;
 using System;
-using System.Linq;
 
-public partial class Main : Node3D
+public partial class Rotation : Marker3D
 {
-    public override void _PhysicsProcess(double delta)
-    {
+    Camera3D camera;
+    Player player;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        camera = (Camera3D)GetNode("PlayerCamera");
+        player = (Player)GetNode("../Player");
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
         var mousePosition = GetViewport().GetMousePosition();
-        var camera = (Camera3D) GetNode("PlayerCamera");
 
         var rayOrigin = camera.ProjectRayOrigin(mousePosition);
         var rayTarget = rayOrigin + camera.ProjectRayNormal(mousePosition) * 2000;
@@ -16,16 +25,17 @@ public partial class Main : Node3D
         var query = PhysicsRayQueryParameters3D.Create(rayOrigin, rayTarget);
         query.CollideWithAreas = true;
         var intersection = spaceState.IntersectRay(query);
-        
+
 
         if (intersection.Count != 0)
         {
             var pos = (Vector3)intersection["position"];
-            var player = (CharacterBody3D)GetNode("Player");
 
             var playerLookPos = new Vector3(pos.X, player.Position.Y, pos.Z);
 
             player.LookAt(playerLookPos, Vector3.Up);
         }
+
+        camera.Position = player.Position + new Vector3(0, 10, 2);
     }
 }
